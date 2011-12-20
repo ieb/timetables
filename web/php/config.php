ca45ff47b50f38e5ddc9ecda7a15b1247a7d0f82
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2011-12-20 15:23:25 +0000

    Fixed blue top bar showing instead of grey w/ Uni branding.
    The web/php/config.php has a $colour var which in a roundabout way is pulled
    down by the client side javascript. The live site leaves it unspecified which
    produces the grey background & University logo (which we want). Ideally I
    suppose play/ should be configured to show a green header as it does with the
    current deployment, but this is hardly a large issue.

--- web/php/config.php
+++ web/php/config.php
@@ -4,6 +4,11 @@ $sysdir = "../../";
 $sysdir_up = "../";
 $pypath = "python";
 $keyfile = "../../secret/hmac-key";
-$colour = '#000099';
-$logfile = '/tmp/mercury.log';
 
+// If $colour is set then it is provided to  mercury-common.js when it requests 
+// php/user.php. The live site's University logo & grey background is in place
+// by default and is left there if no colour is specified here. It should be
+// set to a hash prefixed 24bit hex string if desired, e.g. "#000099"
+$colour = null;
+
+$logfile = '/tmp/mercury.log';
